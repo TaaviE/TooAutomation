19efea7e8cf1667de76737bae0f5cd78efefb46c
TaaviE <taavi.eomae+github@gmail.com>
2017-08-17 01:39:49 +0300

    Separated input and output abstraction.

---
+++ modules/TooInput/TooInput.h
@@ -0,0 +1,64 @@
+/*
+    TooInput - Sensor and other input abstraction layer for TooAutomation
+    Copyright (C) 2017 Taavi E.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file TooInput.h
+ *
+ * This file implements the necessary functionality
+ * that allow nodes to collect data
+ */
+
+#pragma once
+
+#ifndef __TooInput_H__
+#define __TooInput_H__
+
+#include "TooAutomation.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+TODO:
+* Listen for input change (Analog and digital)
+* Monitor for function output change (func *)
+* Report every PREDEFINED_INTERVAL (Analog and digital)
+* Report every PREDEFINED_INTERVAL (func *)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif 
+
+//#ifdef __cplusplus
+//#include "cpp_wrapper.h" 
+//#endif
+
+#endif // __TooInput_H__
---
+++ modules/TooOutput/TooOutput.h
@@ -0,0 +1,61 @@
+/*
+    TooOutput - Actuator and display abstraction layer for TooAutomation
+    Copyright (C) 2017 Taavi E.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file TooOutput.h
+ *
+ * This file implements the necessary functionality
+ * that allow nodes to output and display data
+ */
+
+#pragma once
+
+#ifndef __TooOutput_H__
+#define __TooOutput_H__
+
+#include "TooAutomation.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+TODO:
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif 
+
+//#ifdef __cplusplus
+//#include "cpp_wrapper.h" 
+//#endif
+
+#endif // __TooOutput_H__
